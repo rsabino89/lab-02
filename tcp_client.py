2b3809969159f4929ef6760cf5fbaa769bd5c57f
raymondsabino <rsabin@Ubuntu.myguest.virtualbox.org>
2023-02-02 23:43:42 -0800

    ssh test 1

--- tcp_client.py
+++ tcp_client.py
@@ -8,10 +8,17 @@ use python "input->" function, enter a line of a few letters, such as "abcd"
 import socket
 
 def main():
+    HOST = "127.0.0.1"
+    PORT = 65432
     # TODO: Create a socket and connect it to the server at the designated IP and port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
     # TODO: Get user input and send it to the server using your TCP socket
+        userinput = input("What is your message? ")
+        s.sendall(str.encode(userinput))
     # TODO: Receive a response from the server and close the TCP connection
-    pass
+        data = s.recv(1024)
+    print(f"{data!r}")
 
 
 if __name__ == '__main__':
